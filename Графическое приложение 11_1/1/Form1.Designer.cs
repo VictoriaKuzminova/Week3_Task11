898d5fea118b92fe7f68bccc9c147ec61f7e3ba5
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-12-19 17:46:14 +0300

    Add files via upload

---
+++ Графическое приложение 11_1/1/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.n_textBox = new System.Windows.Forms.TextBox();
+            this.stl_textBox = new System.Windows.Forms.TextBox();
+            this.skl_textBox = new System.Windows.Forms.TextBox();
+            this.res_Button = new System.Windows.Forms.Button();
+            this.res_textBox = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mas_textBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(38, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(359, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите размерность массива (n*n):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(38, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(246, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите номер столбца:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(479, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(268, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Введите значение скаляра:";
+            // 
+            // n_textBox
+            // 
+            this.n_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.n_textBox.Location = new System.Drawing.Point(430, 31);
+            this.n_textBox.Name = "n_textBox";
+            this.n_textBox.Size = new System.Drawing.Size(411, 30);
+            this.n_textBox.TabIndex = 3;
+            // 
+            // stl_textBox
+            // 
+            this.stl_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stl_textBox.Location = new System.Drawing.Point(307, 212);
+            this.stl_textBox.Name = "stl_textBox";
+            this.stl_textBox.Size = new System.Drawing.Size(118, 30);
+            this.stl_textBox.TabIndex = 4;
+            // 
+            // skl_textBox
+            // 
+            this.skl_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.skl_textBox.Location = new System.Drawing.Point(787, 214);
+            this.skl_textBox.Name = "skl_textBox";
+            this.skl_textBox.Size = new System.Drawing.Size(118, 30);
+            this.skl_textBox.TabIndex = 5;
+            // 
+            // res_Button
+            // 
+            this.res_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.res_Button.Location = new System.Drawing.Point(381, 273);
+            this.res_Button.Name = "res_Button";
+            this.res_Button.Size = new System.Drawing.Size(159, 37);
+            this.res_Button.TabIndex = 6;
+            this.res_Button.Text = "Вывести";
+            this.res_Button.UseVisualStyleBackColor = true;
+            this.res_Button.Click += new System.EventHandler(this.res_Button_Click);
+            // 
+            // res_textBox
+            // 
+            this.res_textBox.Location = new System.Drawing.Point(43, 339);
+            this.res_textBox.Name = "res_textBox";
+            this.res_textBox.Size = new System.Drawing.Size(862, 185);
+            this.res_textBox.TabIndex = 7;
+            this.res_textBox.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(38, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(219, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Введите массив (n*n):";
+            // 
+            // mas_textBox
+            // 
+            this.mas_textBox.Location = new System.Drawing.Point(280, 82);
+            this.mas_textBox.Name = "mas_textBox";
+            this.mas_textBox.Size = new System.Drawing.Size(625, 107);
+            this.mas_textBox.TabIndex = 9;
+            this.mas_textBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 576);
+            this.Controls.Add(this.mas_textBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.res_textBox);
+            this.Controls.Add(this.res_Button);
+            this.Controls.Add(this.skl_textBox);
+            this.Controls.Add(this.stl_textBox);
+            this.Controls.Add(this.n_textBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Классы";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox n_textBox;
+        private System.Windows.Forms.TextBox stl_textBox;
+        private System.Windows.Forms.TextBox skl_textBox;
+        private System.Windows.Forms.Button res_Button;
+        private System.Windows.Forms.RichTextBox res_textBox;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.RichTextBox mas_textBox;
+    }
+}
+
---
+++ Графическое приложение 11_1/1/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int n, stl, scal;
+        class Mas
+        {
+            //Поля класса
+            int[,] IntArray;
+            int n;
+            //Конструктор класса
+            public Mas(int n)
+            {
+                this.n = n;
+                IntArray = new int[n, n];
+            }
+            //Метод ввода массива с клавиатуры
+            public void Input(RichTextBox mas_textBox)
+            {
+                if (string.IsNullOrWhiteSpace(mas_textBox.Text))
+                {
+                    MessageBox.Show("Введите элементы массива.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                var sNums = new string[n, n];
+                var arr1 = mas_textBox.Text.Split('\n');
+                if (arr1.Length != n)
+                {
+                    MessageBox.Show("Длина массива не соответствует введенному!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    var arr2 = arr1[i].Split(' ');
+                    if (arr2.Length != n)
+                    {
+                        MessageBox.Show("Длина массива не соответствует введенному!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    for (int j = 0; j < n; j++)
+                    {
+
+                        sNums[i, j] = arr2[j];
+                    }
+                }
+
+                try
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            IntArray[i, j] = int.Parse(sNums[i, j]);
+                        }
+                    }
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Некорректный ввод данных. Попробуйте снова");
+                    return;
+                }
+            }
+            //Метод вывода массива
+            public void Print(RichTextBox res_textBox)
+            {
+                res_textBox.Text += "Mассив: "+'\n';
+                for (int i = 0; i < n; i++, res_textBox.Text += "\n")
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        res_textBox.Text += IntArray[i, j];
+                    }
+                }
+            }
+            //Метод, вычисляющий сумму столбцов
+            public void Sum(int stl, RichTextBox res_textBox)
+            {
+                int sum = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    sum += IntArray[i, stl];
+                }
+                res_textBox.Text += "Сумма = " + sum+'\n';
+
+            }
+            //позволяющее вычислить количество нулевых элементов в массиве (доступное только для чтения);
+            public int Kol_Zero_Element
+            {
+                get
+                {
+                    int count = 0;
+                    for (int i = 0; i < n; i++)
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (IntArray[i, j] == 0)
+                                count++;
+                        }
+                    return count;
+                }
+            }
+            // позволяющее установить значение всех элементы главной диагонали массива равное скаляру (доступное только для записи).
+            public int Scal
+            {
+                set
+                {
+                    for (int i = 0; i < n; i++)
+                        IntArray[i, i] = value;
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void res_Button_Click(object sender, EventArgs e)
+        {
+            if (n_textBox.Text == "" || mas_textBox.Text == "" || skl_textBox.Text == "" || stl_textBox.Text == "")
+            {
+                MessageBox.Show("Введены не все данные");
+            }
+            else
+            {
+                res_textBox.Text = "";
+                try
+                {
+                    n = int.Parse(n_textBox.Text);
+                    if (n <= 0)
+                    {
+                        MessageBox.Show("Минимальное значение 1");
+                        return;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Некорректный ввод");
+                    return;
+                }
+            }
+            Mas mas = new Mas(n);
+            mas.Input(mas_textBox);
+            mas.Print(res_textBox);
+            try
+            {
+                stl = int.Parse(stl_textBox.Text);
+                if (stl < 0)
+                {
+                    MessageBox.Show("Число должно быть положительным");
+                    return;
+                }
+                if (stl > (n - 1))
+                {
+                    MessageBox.Show("Значение должно быть меньше");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Некорректный ввод");
+                return;
+            }
+            mas.Sum(stl, res_textBox);
+            res_textBox.Text += "Количество нулевых элементов: " + mas.Kol_Zero_Element+'\n';
+            try
+            {
+                scal = int.Parse(skl_textBox.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Некорректный ввод");
+                return;
+            }
+            res_textBox.Text += "Все элементы главной диагонали равны скаляру: "+'\n';
+            mas.Scal = scal;
+            mas.Print(res_textBox);
+        }
+    }
+}
+
